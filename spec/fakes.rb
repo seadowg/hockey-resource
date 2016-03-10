4332edf43c1a489d22672d77267bd7e2c69b1e4e
Callum Stott <cstott@pivotal.io>
2016-03-10 16:57:11 +0000

    Move fakes and utils to their own file

---
+++ spec/fakes.rb
@@ -0,0 +1,64 @@
+class FakeIO
+  def initialize
+    @buffer = []
+  end
+
+  def write(input)
+    @buffer << input
+  end
+
+  def read
+    @buffer.pop
+  end
+end
+
+class FakeRestClient
+  def initialize
+    @response_map = {}
+    @posts = []
+  end
+
+  def add_response(url, response)
+    @response_map[url] = response
+  end
+
+  def post(url, body, headers = {})
+    @posts << FakePost.new(url, body, headers)
+
+    if @response_map[url]
+      @response_map[url]
+    else
+      ""
+    end
+  end
+
+  def posts
+    @posts
+  end
+end
+
+class FakePost
+  attr_reader :url
+  attr_reader :headers
+  attr_reader :body
+
+  def initialize(url, body, headers)
+    @url = url
+    @headers = headers
+    @body = body
+  end
+end
+
+class FakeFS
+  def get(path)
+    FakeFile.new(path)
+  end
+end
+
+class FakeFile
+  attr_reader :path
+
+  def initialize(path)
+    @path = path
+  end
+end
